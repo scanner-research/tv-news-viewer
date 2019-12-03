2c4aadcaa9259a2613edd7dca045f7ab8881aa17
James Hong <james.hong@cs.stanford.edu>
2019-12-03 13:09:40 +0100

    Show more button, fix allDataExample

--- static/js/table.js
+++ static/js/table.js
@@ -1,16 +1,26 @@
+const LONG_PREVIEW_COUNT = 5;
+
 function toggleDataVis(e) {
   let cell = $(e).parent();
   cell.find('.long-data-value').toggle();
   cell.find('.toggle-data').text(
-    cell.find('.long-data-value:visible').length > 0 ? 'hide' : 'show'
+    cell.find('.long-data-value:visible').length > 0 ? 'hide' : 'show more'
   );
 }
 
 function renderLongDataValue(x) {
   if (x.length > 250) {
-    var html = $('<a class="toggle-data" href="javascript:void(0);" onclick="toggleDataVis(this);" />').text('show').prop('outerHTML');
-    html += $('<span class="long-data-value" />').text(x).prop('outerHTML');
-    return html;
+    let tokens = x.split(',').map($.trim);
+    if (tokens.length < LONG_PREVIEW_COUNT) {
+      return x;
+    } else {
+      var html = tokens.slice(0, LONG_PREVIEW_COUNT).join(', ');
+      html += $('<span class="long-data-value" />').text(
+        ', ' + tokens.slice(LONG_PREVIEW_COUNT).join(', ')
+      ).prop('outerHTML');
+      html += $('<a class="toggle-data" href="javascript:void(0);" onclick="toggleDataVis(this);" />').text('show more').prop('outerHTML');
+      return html;
+    }
   } else {
     return x;
   }
