dc2c10f3ee2f0a29ab05273e889b7532a1971ede
James Hong <james.hong@cs.stanford.edu>
2019-12-12 17:20:49 -0800

    Allow embedded editor

--- static/js/embed.js
+++ static/js/embed.js
@@ -1,25 +1,142 @@
 const CHART_H_SLACK = 50;
 const CHART_MIN_WIDTH = 300;
-let params = (new URL(document.location)).searchParams;
-let hide_legend = params.get('hideLegend') == 1;
-let hide_tooltip = params.get('hideTooltip') == 1;
-let hide_mean = params.get('showMean') != 1;
-let data_str = params.get('data');
-let data = JSON.parse(data_str);
 
-function checkDataVersion() {
-  let chart_info_div = $('#chartInfo');
+function checkDataVersion(params) {
   if (params.get('dataVersion')) {
-    let version_id = decodeURIComponent(params.get('dataVersion'));
+    let version_id = params.get('dataVersion');
     if (DATA_VERSION_ID != version_id) {
-      chart_info_div.append(
-        $('<span>').html(`<b>Warning:</b> the requested data version has changed from <b>${version_id}</b> to <b>${DATA_VERSION_ID}</b>. The following chart may have changed.`)
-      ).show();
+      return $('<span>').html(`<b>Warning:</b> the requested data version has changed from <b>${version_id}</b> to <b>${DATA_VERSION_ID}</b>. The following chart may have changed.`);
     }
   }
+  return null;
 }
 
-function renderChartInfo(lines) {
+function getChartDimensions() {
+  let width = Math.max($(document).width() - CHART_H_SLACK, CHART_MIN_WIDTH);
+  let height = $(document).height() - $('.chart-info').height() - 20;
+  return {width: width, height: height};
+}
+
+function clearChart() {
+  $('#chart').empty();
+}
+
+function reset() {
+  // Remove all rows except one
+  $('tr[name="query"]').each(function() {
+    if ($(this).index() > 0) {
+      removeRow($(this));
+    }
+  });
+
+  // Reset the last code editor
+  Object.values(CODE_EDITORS).forEach(e => e.setValue(''));
+
+  // Reset to defaults
+  $('#aggregateBy').val(DEFAULT_AGGREGATE_BY).trigger("chosen:updated");
+  $('#startDate').val(toDatepickerStr(DEFAULT_START_DATE));
+  $('#endDate').val(toDatepickerStr(DEFAULT_END_DATE));
+  clearChart();
+}
+
+function search() {
+  clearChart();
+
+  var chart_options;
+  try {
+    chart_options = getChartOptions();
+  } catch (e) {
+    alertAndThrow(e.message);
+  }
+
+  let lines = getRawQueries().map(
+    raw_query => {
+      var parsed_query;
+      try {
+        parsed_query = new SearchableQuery(raw_query.text, false);
+      } catch (e) {
+        alertAndThrow(e.message);
+      }
+      return {color: raw_query.color, query: parsed_query};
+    }
+  );
+
+  $('#loadingText').show();
+  let indexed_search_results = [];
+  function onDone() {
+    $('#loadingText').hide();
+    indexed_search_results.sort();
+    new Chart(
+      chart_options, indexed_search_results.map(([i, v]) => v),
+      getChartDimensions()
+    ).load('#chart', {
+      show_tooltip: true, show_mean: false
+    });
+  }
+
+  Promise.all(lines.map((line, i) => {
+    console.log('Executing query:', line.query);
+
+    var errored = false;
+    function onError(xhr, status, error) {
+      if (errored) {
+        return;
+      }
+      var msg;
+      try {
+        msg = `${status}=${JSON.parse(xhr.responseText).message}`;
+      } catch {
+        msg = `${status}=${error}`;
+      }
+      alert(`[Query failed. The chart is incomplete.]\n\n${line.query.query}\n\n${msg}`);
+      console.log('Failed:', line.query, xhr);
+      errored = true;
+    }
+
+    return line.query.search(
+      chart_options,
+      result => indexed_search_results.push([i, [line.color, result]]),
+      onError);
+  })).then(onDone).catch(onDone);
+}
+
+function initializeDynamic(params) {
+  ENABLE_QUERY_BUILDER = false;
+
+  $('.chart-info').show();
+  $('#searchTable').show();
+
+  addParsingMode('tvnews', {no_prefix: true, check_values: true});
+  addCodeHintHelper('tvnews');
+
+  var loaded = false;
+  if (params.get('data')) {
+    try {
+      let data = JSON.parse(params.get('data'));
+      initChartOptions(data.options);
+      data.queries.forEach(query => addRow(query));
+      search();
+      loaded = true;
+    } catch (e) {
+      alert('Invalid data in url. Please make sure you copied it correctly. Loading defaults instead.');
+      console.log('Unable to load:', e);
+    }
+  }
+  if (!loaded) {
+    initChartOptions();
+    addRow({text: ''});
+    try {
+      search();
+    } catch (e) {};
+  }
+
+  $(".chosen-select").chosen({width: 'auto'});
+  $('#searchTable .add-row-btn').click(() => {addRow();});
+  $('#searchButton').click(search);
+  $('#resetButton').click(reset);
+}
+
+function getStaticLegend(lines) {
   let legend = $('<div>').append(lines.map(line => {
     var entry;
     if (line.query.alias) {
@@ -39,57 +156,79 @@ function renderChartInfo(lines) {
       $('<span>').addClass('legend-text').append(entry)
     );
   }));
-  $('#chartInfo').append(legend);
+  return legend;
 }
 
-let lines = data.queries.map(raw_query => {
-  var parsed;
-  try {
-    parsed_query = new SearchableQuery(raw_query.text, false);
-  } catch (e) {
-    alertAndThrow(e.message);
-  }
-  return {color: raw_query.color, query: parsed_query};
-});
+function initializeStatic(params) {
+  $('#loadingText').show();
+  let hide_legend = params.get('hideLegend') == 1;
+  let hide_tooltip = params.get('hideTooltip') == 1;
+  let hide_mean = params.get('showMean') != 1;
+  let data_str = params.get('data');
+  let data = JSON.parse(data_str);
 
-$('#shade').show();
-$('#search').prop('disabled', true);
-
-let search_results = [];
-function onDone() {
-  checkDataVersion();
-  if (!hide_legend) {
-    renderChartInfo(lines);
-  }
-
-  let width = $(document).width();
-  let height = $(document).height() - $('#chartInfo').height() - 20;
-  new Chart(
-    data.options, search_results,
-    {width: Math.max(width - CHART_H_SLACK, CHART_MIN_WIDTH), height: height}
-  ).load('#chart', {
-    show_tooltip: !hide_tooltip, show_mean: !hide_mean,
-    href: `//${SERVER_HOST}/?data=` + encodeURIComponent(data_str)
-  });
-  $('#loadingText').hide();
-};
-
-Promise.all(lines.map(line => {
-  console.log('Executing query:', line.query);
-
-  function onError(xhr) {
-    var msg;
+  let lines = data.queries.map(raw_query => {
+    var parsed;
     try {
-      msg = JSON.parse(xhr.responseText).message;
-    } catch {
-      msg = 'Unknown server error';
+      parsed_query = new SearchableQuery(raw_query.text, false);
+    } catch (e) {
+      alertAndThrow(e.message);
     }
-    alert(`[Query failed. The chart may be incomplete.]\n\n${msg}`);
-    console.log('Failed:', line, xhr);
-  }
+    return {color: raw_query.color, query: parsed_query};
+  });
 
-  return line.query.search(
-    data.options,
-    result => search_results.push([line.color, result]),
-    onError);
-})).then(onDone).catch(onDone);
+  $('#shade').show();
+  $('#search').prop('disabled', true);
+
+  let search_results = [];
+  function onDone() {
+    let chart_info_div = $('.chart-info');
+    if (!hide_legend) {
+      chart_info_div.append(getStaticLegend(lines));
+    }
+    chart_info_div.show();
+    new Chart(
+      data.options, search_results, getChartDimensions(),
+    ).load('#chart', {
+      show_tooltip: !hide_tooltip, show_mean: !hide_mean,
+      href: `//${SERVER_HOST}/?data=` + encodeURIComponent(data_str)
+    });
+    $('#loadingText').hide();
+  };
+
+  Promise.all(lines.map(line => {
+    console.log('Executing query:', line.query);
+
+    function onError(xhr) {
+      var msg;
+      try {
+        msg = JSON.parse(xhr.responseText).message;
+      } catch {
+        msg = 'Unknown server error';
+      }
+      alert(`[Query failed. The chart may be incomplete.]\n\n${msg}`);
+      console.log('Failed:', line, xhr);
+    }
+
+    return line.query.search(
+      data.options,
+      result => search_results.push([line.color, result]),
+      onError);
+  })).then(onDone).catch(onDone);
+}
+
+function initialize() {
+  let params = (new URL(document.location)).searchParams;
+  let chart_info_div = $('.chart-info');
+  chart_info_div.prepend(checkDataVersion(params));
+
+  let enable_editor = params.get('edit') == 1;
+  if (enable_editor) {
+    initializeDynamic(params);
+  } else {
+    initializeStatic(params);
+  }
+}
+
+/* Load widget */
+initialize();
